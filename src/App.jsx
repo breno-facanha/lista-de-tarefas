fda359554bc7f8fc5d33905431a118153f0f859e
Breno Façanha <brenofacanha.ti@gmail.com>
2025-02-02 19:44:44 -0300

    criando o primeiro componente Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Tasks from './components/Tasks'
 
 function App() {
-
-  let [ msg, setMsg ] = useState('Olá mundo !')
-
-  let mensagem = ' Olá, mundo ! Sou Breno Façanha, Dev Júnior !'
  
   return (
    <div className='bg-red-400 w-full h-screen flex justify-center flex-col items-center border gap-2'>
-     {msg}
-    <button 
-      className='border-2 border-amber-500 rounded-md w-12 bg-amber-500 text-amber-100 cursor-pointer w-auto px-2'
-      onClick={() => setMsg(mensagem)}  
-    >Mudar Mensagem</button>
+    <h1>Gerenciador de Tarefas</h1>
+    <Tasks />
    </div>
   )
 }
