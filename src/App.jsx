965d197d1b91144d506ca639b5cdefd86a11be51
Breno Façanha <brenofacanha.ti@gmail.com>
2025-02-02 19:55:55 -0300

    criado o componente AddTask, deeltado o arquivo App.css e ajustado o estilo no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import AddTask from './components/AddTask'
 import Tasks from './components/Tasks'
 
 function App() {
  
   return (
-   <div className='bg-red-400 w-full h-screen flex justify-center flex-col items-center border gap-2'>
-    <h1>Gerenciador de Tarefas</h1>
-    <Tasks />
+   <div className='bg-slate-500 w-full h-screen flex  flex-col items-center border gap-2 p-6'>
+    <div className='w-[500px '>
+      <h1 className='text-3xl text-slate-100 font-bold text-center'>Gerenciador de Tarefas</h1>
+      <AddTask />
+      <Tasks />
+    </div>
    </div>
   )
 }
