6b9f2fa0d48bb24b6be67959a7977bb999f3e212
Breno Façanha <brenofacanha.ti@gmail.com>
2025-02-03 21:29:47 -0300

    adicionando 2 inputs e button de adcionar task já estilizados

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,19 @@
 export default function AddTask(){
     return (
-        <h1>Addtask</h1>
+       <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+        <input 
+            type="text" 
+            placeholder="Digite o titulo da tarefa"
+            className="border border-slate-300 outline-slate-400 bg-amber-50 px-4 py-2 rounded-md"
+        />
+       
+        <input 
+            type="text" 
+            placeholder="Digite a descrição da tarefa"
+            className="border border-slate-300 outline-slate-400 bg-amber-50 px-4 py-2 rounded-md"
+        />   
+        
+        <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+       </div>
     )
 }
